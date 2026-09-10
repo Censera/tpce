96fa866cee09ca1880b0457b0093aeac545fbb30
Censera <Censera@proton.me>
2026-09-10 19:48:48 +0100

    Fix Paper YamlConfiguration import

--- src/main/java/com/censera/tpce/HomeStore.java
+++ src/main/java/com/censera/tpce/HomeStore.java
@@ -4,7 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.YamlConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
